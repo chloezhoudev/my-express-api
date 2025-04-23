f5fd923fc58954eda4518b0ae41f8f3c4ab57037
Chloe Zhou <chloez0607@gmail.com>
2025-04-23 20:13:06 +0800

    feat: set up some routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import morgan from 'morgan';
+import router from './router';
+
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.use('/api', router);
 
 export default app;
